0b152e53fd4897b7b3c6c81d90730ea7d0c2c613
podhmo <ababjam61+github@gmail.com>
2016-12-24 19:12:11 +0900

    scaffold

---
+++ README.rst
@@ -0,0 +1,3 @@
+swagger-marshmallow-codegen
+========================================
+
---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
+
+install_requires = [
+    'dictknife[load]',
+]
+
+
+docs_extras = [
+]
+
+tests_require = [
+]
+
+testing_extras = tests_require + [
+]
+
+setup(name='swagger-marshmallow-codegen',
+      version='0.0',
+      description='generating marshmallow\'s schema from swagger definition file',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      keywords='swagger,codegen,marshmallow,code-generation,schema',
+      author="podhmo",
+      author_email="ababjam61@gmail.com",
+      url="https://github.com/podhmo/swagger-marshmallow-codegen",
+      packages=find_packages(exclude=["swagger_marshmallow_codegen.tests"]),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'testing': testing_extras,
+          'docs': docs_extras,
+      },
+      tests_require=tests_require,
+      test_suite="swagger_marshmallow_codegen.tests",
+      entry_points="""
+""")
+
