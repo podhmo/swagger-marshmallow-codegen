75dc06178f833aca7e6b950b7831d9a6ba9cf06f
podhmo <ababjam61+github@gmail.com>
2017-01-10 15:08:18 +0900

    typo

--- swagger_marshmallow_codegen/fields.py
+++ swagger_marshmallow_codegen/fields.py
@@ -6,18 +6,18 @@ class Date(fields.Date):
     def _deserialize(self, value, attr, data):
         if isinstance(value, datetime.date):
             return value
-        return super(value, attr, data)
+        return super()._deserialize(value, attr, data)
 
 
 class DateTime(fields.Date):
     def _deserialize(self, value, attr, data):
         if isinstance(value, datetime.datetime):
             return value
-        return super(value, attr, data)
+        return super()._deserialize(value, attr, data)
 
 
 class Time(fields.Time):
     def _deserialize(self, value, attr, data):
         if isinstance(value, datetime.time):
             return value
-        return super(value, attr, data)
+        return super()._deserialize(value, attr, data)
