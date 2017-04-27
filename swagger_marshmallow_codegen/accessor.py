90ab5f9234951d1c3efc180d54bbbc0bc0dfb636
podhmo <ababjam61+github@gmail.com>
2017-04-27 16:01:05 +0900

    x-nullable support fixed #18

--- swagger_marshmallow_codegen/accessor.py
+++ swagger_marshmallow_codegen/accessor.py
@@ -55,10 +55,14 @@ class Accessor(object):
             logger.debug("    resolve: dump_only=True")
             opts["dump_only"] = True
         typ = field.get("type")
+
         if isinstance(typ, (list, tuple)):
             for typ in typ:
                 if typ is None:
                     opts["allow_none"] = True
+        if field.get("x-nullable") or field.get("nullable"):
+            opts["allow_none"] = True
+
         validators = self.resolver.resolve_validators_on_property(c, field)
         if validators:
             opts["validate"] = validators
--- swagger_marshmallow_codegen/tests/test_it.py
+++ swagger_marshmallow_codegen/tests/test_it.py
@@ -90,6 +90,7 @@ class CodegenTests(DiffTestCase):
             ("./src/00reserved.yaml", "./dst/00reserved.py"),
             ("./src/00typearray.yaml", "./dst/00typearray.py"),
             ("./src/00additional.yaml", "./dst/00additional.py"),
+            ("./src/00nullable.yaml", "./dst/00nullable.py"),
             # ("./src/00patternProperties.yaml", "./dst/00patternProperties.py"),  not supported yet
         ]
         for src_file, dst_file in candidates:
