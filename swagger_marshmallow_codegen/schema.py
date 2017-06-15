5452317f38365bdb04d078091dbbe6aa5664984f
podhmo <ababjam61+github@gmail.com>
2017-06-15 16:31:48 +0900

    change PrimitiveValueSchema implementation

--- swagger_marshmallow_codegen/schema.py
+++ swagger_marshmallow_codegen/schema.py
@@ -1,55 +1,31 @@
 from marshmallow import Schema, SchemaOpts, fields
-from marshmallow import pre_load, post_load, pre_dump, post_dump
+from marshmallow import pre_load, pre_dump
 from marshmallow import UnmarshalResult, MarshalResult
 
 
-class PrimitiveValueSchema(Schema):
-    """tempoary primitive value schema"""
+class PrimitiveValueSchema:
+    schema_class = None
+    key = "value"
+    missing = None
 
-    v = None
-    default = {}
+    def __init__(self, *args, **kwargs):
+        self.schema = self.__class__.schema_class(*args, **kwargs)
 
-    @pre_load
-    def to_dict(self, v):
-        return {"v": v}
+    def load(self, value):  # don't support many
+        data = {self.key: value}
+        r, errors = self.schema.load(data)
+        return UnmarshalResult(
+            data=r.get(self.key) or self.missing,
+            errors=errors.get(self.key) or errors,
+        )
 
-    @post_load
-    def to_v(self, v):
-        return v.get("v")
-
-    @pre_dump
-    def to_dict2(self, v):
-        return {"v": v}
-
-    @post_dump
-    def to_v2(self, v):
-        return v.get("v")
-
-    def load(self, data, many=None, partial=None):
-        r = super().load(data, many=many, partial=partial)
-        if r.errors:
-            if self.many if many is None else bool(many):
-                # xxx:
-                data = [d.get("v") or self.default for d in r.data]
-                errors = {k: v["v"] for k, v in r.errors.items()}
-            else:
-                data = data["v"]
-                errors = r.errors["v"]
-            return UnmarshalResult(data=data, errors=errors)
-        return r
-
-    def dump(self, obj, many=None, update_fields=True, **kwargs):
-        r = super().dump(obj, many=many, update_fields=update_fields, **kwargs)
-        if r.errors:
-            if self.many if many is None else bool(many):
-                # xxx:
-                data = [d.get("v") or self.default for d in r.data]
-                errors = {k: v["v"] for k, v in r.errors.items()}
-            else:
-                data = data["v"]
-                errors = r.errors["v"]
-            return MarshalResult(data=data, errors=errors)
-        return r
+    def dump(self, value):  # don't support many
+        data = {self.key: value}
+        r, errors = self.schema.dump(data, update_fields=False)
+        return MarshalResult(
+            data=r.get(self.key) or self.missing,
+            errors=errors.get(self.key) or errors,
+        )
 
 
 class AdditionalPropertiesOpts(SchemaOpts):
@@ -64,7 +40,7 @@ class AdditionalPropertiesSchema(Schema):
 
     class MySchema(AdditionalPropertiesSchema):
         class Meta:
-            additional
+            additional_field = fields.Integer()
     """
 
     OPTIONS_CLASS = AdditionalPropertiesOpts
