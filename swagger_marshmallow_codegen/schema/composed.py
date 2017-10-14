7e89b7e7e2e2b25983431f43d04b089cd52b4dfb
podhmo <ababjam61+github@gmail.com>
2017-10-14 22:37:45 +0900

    oneof

---
+++ swagger_marshmallow_codegen/schema/composed.py
@@ -0,0 +1,141 @@
+from collections import namedtuple
+from marshmallow import Schema, ValidationError
+from marshmallow import marshalling
+from prestring.utils import reify
+
+StrategyContext = namedtuple("StrategyContext", "data, schemas, results, errors, compacted")
+
+
+class OneOfSchema(Schema):
+    schema_clsses = []
+
+    def __init__(self, *args, **kwargs):
+        strict = kwargs.pop("strict", None)
+        many = kwargs.pop("many", None)
+        self.schemas = [
+            cls(*args, strict=False, many=False, **kwargs) for cls in self.schema_classes
+        ]
+        super().__init__(strict=strict, many=many)
+
+        self._marshal = ComposedMarshaller(self.schemas, self.final_check)
+        self._unmarshal = ComposedUnmarshaller(self.schemas, self.final_check)
+
+    def final_check(self, sctx):
+        compacted = sctx.compacted
+        if len(compacted) == 1:
+            return compacted[0], {}
+        elif len(compacted) > 1:
+            for other in compacted[1:]:
+                compacted[0].update(other)
+
+            satisfied = []
+            for i, err in enumerate(sctx.errors):
+                if not err:
+                    satisfied.append(sctx.schemas[i].__class__.__name__)
+            return compacted[0], {
+                "_schema": ["satisfied both of {}, not only one".format(satisfied)]
+            }
+        else:
+            results = sctx.results
+            for other in results[1:]:
+                results[0].update(other)
+            return sctx.data if not results else results[0], {
+                "_schema":
+                ["not matched, any of {}".format([s.__class__.__name__ for s in self.schemas])]
+            }
+
+
+def run_many(data, fn, **kwargs):
+    results = []
+    errors = {}
+    for i, d in enumerate(data):
+        r, err = fn(d, **kwargs)
+        results.append(r)
+        if err:
+            errors[i] = err
+    return results, errors
+
+
+class ComposedMarshaller(marshalling.Marshaller):
+    def __init__(self, schemas, final_check):
+        super().__init__()
+        self.schemas = schemas
+        self.final_check = final_check
+
+    @reify
+    def mapping(self):
+        d = {}
+        for s in self.schemas:
+            sig = tuple(sorted([name for name, f in s.fields.items() if f.required]))
+            d[sig] = s
+        return d
+
+    def find_matched_schemas(self, data):
+        r = []
+        for signature, s in self.mapping.items():
+            if all(k in data for k in signature):
+                r.append(s)
+        return r
+
+    def marshall(self, obj, fields_dict, *, many, accessor, dict_class, index_errors, index=None):
+        self.reset_errors()
+        if many:
+            d, errors = run_many(obj, self._marshall_one)
+        else:
+            d, errors = self._marshall_one(obj)
+        self.errors = errors
+        if errors:
+            raise ValidationError(errors, data=d)
+        return d
+
+    __call__ = marshall
+
+    def _marshall_one(self, data):
+        results = []
+        errors = []
+        compacted = []
+        schemas = self.find_matched_schemas(data)
+        for s in schemas:
+            r, err = s.dump(data, update_fields=False)
+            if not err:
+                compacted.append(r)
+            results.append(r)
+            errors.append(err)
+        sctx = StrategyContext(
+            data=data, results=results, errors=errors, schemas=schemas, compacted=compacted
+        )
+        return self.final_check(sctx)
+
+
+class ComposedUnmarshaller(marshalling.Unmarshaller):
+    def __init__(self, schemas, final_check):
+        super().__init__()
+        self.schemas = schemas
+        self.final_check = final_check
+
+    def unmarshall(self, data, fields, *, many, partial, dict_class, index_errors):
+        self.reset_errors()
+        if many:
+            d, errors = run_many(data, self._unmarshall_one, partial=partial)
+        else:
+            d, errors = self._unmarshall_one(data, partial=partial)
+        self.errors = errors
+        return d
+
+    __call__ = unmarshall
+
+    def _unmarshall_one(self, data, *, partial):
+        results = []
+        errors = []
+        compacted = []
+        schemas = self.schemas
+        for s in schemas:
+            r, err = s.load(data, partial=partial)
+            if not err:
+                compacted.append(r)
+            results.append(r)
+            errors.append(err)
+        sctx = StrategyContext(
+            data=data, results=results, errors=errors, schemas=schemas, compacted=compacted
+        )
+        return self.final_check(sctx)
---
+++ swagger_marshmallow_codegen/tests/schema/test_oneof.py
@@ -0,0 +1,346 @@
+import unittest
+from testmarker import mark
+from collections import namedtuple
+from marshmallow import (
+    Schema,
+    fields,
+)
+
+
+class Person(Schema):
+    name = fields.String(required=True)
+    age = fields.Integer(required=True)
+
+
+class Group(Schema):
+    name = fields.String(required=True)
+    members = fields.List(fields.Nested('Person'), required=True)
+
+
+@mark.oneof
+class LoadTests(unittest.TestCase):
+    def _getTargetClass(self):
+        from swagger_marshmallow_codegen.schema import OneOfSchema
+
+        class S(OneOfSchema):
+            schema_classes = (Person, Group)
+
+        return S
+
+    def test_many_false(self):
+        from marshmallow import UnmarshalResult
+        S = self._getTargetClass()
+        C = namedtuple("C", "data, expected, msg")
+        candidates = [
+            C(
+                msg="person missing",
+                data=dict(age="10"),
+                expected=UnmarshalResult(
+                    data=dict(age=10),
+                    errors=dict(_schema=["not matched, any of ['Person', 'Group']"])
+                ),
+            ),
+            C(
+                msg="person ok",
+                data=dict(name="foo", age="10"),
+                expected=UnmarshalResult(data=dict(name="foo", age=10), errors={})
+            ),
+            C(
+                msg="group ok",
+                data=dict(name="A", members=[]),
+                expected=UnmarshalResult(data=dict(name="A", members=[]), errors={}),
+            ),
+            C(
+                msg="group ok2",
+                data=dict(name="A", members=[dict(name="foo", age="10")]),
+                expected=UnmarshalResult(
+                    data=dict(name="A", members=[dict(name="foo", age=10)]), errors={}
+                ),
+            ),
+            C(
+                msg="both",
+                data=dict(age="10", name="A", members=[]),
+                expected=UnmarshalResult(
+                    data=dict(age=10, name="A", members=[]),
+                    errors=dict(_schema=["satisfied both of ['Person', 'Group'], not only one"])
+                ),
+            ),
+        ]
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                s = S()
+                got = s.load(c.data)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
+
+    def test_many_true(self):
+        from marshmallow import UnmarshalResult
+        S = self._getTargetClass()
+        C = namedtuple("C", "data, expected, msg")
+        candidates = [
+            C(
+                msg="person missing",
+                data=[dict(age="10")],
+                expected=UnmarshalResult(
+                    data=[dict(age=10)],
+                    errors={0: dict(_schema=["not matched, any of ['Person', 'Group']"])}
+                ),
+            ),
+            C(
+                msg="person ok",
+                data=[dict(name="foo", age="10")],
+                expected=UnmarshalResult(data=[dict(name="foo", age=10)], errors={})
+            ),
+            C(
+                msg="group ok",
+                data=[dict(name="A", members=[])],
+                expected=UnmarshalResult(data=[dict(name="A", members=[])], errors={}),
+            ),
+            C(
+                msg="group ok2",
+                data=[dict(name="A", members=[dict(name="foo", age="10")])],
+                expected=UnmarshalResult(
+                    data=[dict(name="A", members=[dict(name="foo", age=10)])], errors={}
+                ),
+            ),
+            C(
+                msg="ng items[1]",
+                data=[dict(name="foo", age="10"),
+                      dict(name="bar"),
+                      dict(name="boo", age="10")],
+                expected=UnmarshalResult(
+                    data=[dict(name="foo", age=10),
+                          dict(name="bar"),
+                          dict(name="boo", age=10)],
+                    errors={1: {
+                        '_schema': ["not matched, any of ['Person', 'Group']"]
+                    }},
+                ),
+            )
+        ]
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                s = S()
+                got = s.load(c.data, many=True)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
+
+    def test_nested(self):
+        from marshmallow import UnmarshalResult, fields, Schema
+
+        class S(Schema):
+            target = fields.Nested(self._getTargetClass())
+
+        C = namedtuple("C", "data, expected, msg")
+        candidates = [
+            C(
+                msg="person missing",
+                data=dict(target=dict(age="10")),
+                expected=UnmarshalResult(
+                    data=dict(target=dict(age=10)),
+                    errors=dict(target=dict(_schema=["not matched, any of ['Person', 'Group']"]))
+                ),
+            ),
+            C(
+                msg="person ok",
+                data=dict(target=dict(name="foo", age="10")),
+                expected=UnmarshalResult(
+                    data=dict(target=dict(name="foo", age=10)),
+                    errors={},
+                ),
+            ),
+        ]
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                s = S()
+                got = s.load(c.data)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
+
+    def test_decorator(self):
+        from marshmallow import UnmarshalResult
+        from marshmallow import pre_load, post_load
+
+        class S(self._getTargetClass()):
+            @pre_load
+            def lowercase(self, data):
+                return {k.lower(): v for k, v in data.items()}
+
+            @post_load
+            def wrap(self, data):
+                return {"wrap": data}
+
+        C = namedtuple("C", "data, expected, msg, many")
+        candidates = [
+            C(
+                msg="person missing",
+                many=False,
+                data=dict(age="10"),
+                expected=UnmarshalResult(
+                    data=dict(age=10),
+                    errors=dict(_schema=["not matched, any of ['Person', 'Group']"])
+                ),
+            ),
+            C(
+                msg="person ok",
+                many=False,
+                data=dict(NAME="foo", AGE="10"),
+                expected=UnmarshalResult(data=dict(wrap=dict(name="foo", age=10)), errors={})
+            ),
+            C(
+                msg="people ok",
+                many=True,
+                data=[dict(NAME="foo", AGE="10"),
+                      dict(NAME="bar", AGE="10")],
+                expected=UnmarshalResult(
+                    [dict(wrap=dict(name="foo", age=10)),
+                     dict(wrap=dict(name="bar", age=10))],
+                    errors={}
+                )
+            ),
+        ]
+        s = S()
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                got = s.load(c.data, many=c.many)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
+
+    def test_load_with_unmatched(self):
+        from marshmallow import UnmarshalResult, fields, Schema
+        C = namedtuple("C", "schema, msg, opts, expected")
+
+        S1 = self._getTargetClass()
+
+        class S2(Schema):
+            target = fields.Nested(self._getTargetClass())
+
+        candidates = [
+            C(
+                schema=Person,
+                msg="default marshmallow",
+                opts={"many": True},
+                expected=UnmarshalResult(data=[], errors={})
+            ),
+            C(
+                schema=S1,
+                msg="default marshmallow",
+                opts={"many": True},
+                expected=UnmarshalResult(data=[], errors={})
+            ),
+            C(
+                schema=S1,
+                msg="default marshmallow",
+                opts={"many": True},
+                expected=UnmarshalResult(data=[], errors={})
+            )
+        ]
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                got = c.schema(**c.opts).load({})
+                self.assertEqual(got, c.expected)
+
+
+@mark.oneof
+class DumpTests(unittest.TestCase):
+    def _getTargetClass(self):
+        from swagger_marshmallow_codegen.schema import OneOfSchema
+
+        class S(OneOfSchema):
+            schema_classes = (Person, Group)
+
+        return S
+
+    def test_many_false(self):
+        from marshmallow import MarshalResult
+        S = self._getTargetClass()
+        C = namedtuple("C", "data, expected, msg")
+        candidates = [
+            C(
+                msg="person missing",
+                data=dict(age=10),
+                expected=MarshalResult(
+                    data=dict(age=10),
+                    errors=dict(_schema=["not matched, any of ['Person', 'Group']"])
+                ),
+            ),
+            C(
+                msg="person ok",
+                data=dict(name="foo", age=10),
+                expected=MarshalResult(data=dict(name="foo", age=10), errors={})
+            ),
+            C(
+                msg="group ok",
+                data=dict(name="A", members=[]),
+                expected=MarshalResult(data=dict(name="A", members=[]), errors={}),
+            ),
+            C(
+                msg="group ok2",
+                data=dict(name="A", members=[dict(name="foo", age=10)]),
+                expected=MarshalResult(
+                    data=dict(name="A", members=[dict(name="foo", age=10)]), errors={}
+                ),
+            ),
+            C(
+                msg="both",
+                data=dict(age="10", name="A", members=[]),
+                expected=MarshalResult(
+                    data=dict(age=10, name="A", members=[]),
+                    errors=dict(_schema=["satisfied both of ['Person', 'Group'], not only one"])
+                ),
+            ),
+        ]
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                s = S()
+                got = s.dump(c.data)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
+
+    def test_decorator(self):
+        from marshmallow import MarshalResult
+        from marshmallow import pre_dump, post_dump
+
+        class S(self._getTargetClass()):
+            @pre_dump
+            def lowercase(self, data):
+                return {k.lower(): v for k, v in data.items()}
+
+            @post_dump
+            def wrap(self, data):
+                return {"wrap": data}
+
+        C = namedtuple("C", "data, expected, msg, many")
+        candidates = [
+            C(
+                msg="person missing",
+                many=False,
+                data=dict(AGE="10"),
+                expected=MarshalResult(
+                    data=dict(age="10"),
+                    errors=dict(_schema=["not matched, any of ['Person', 'Group']"])
+                ),
+            ),
+            C(
+                msg="person ok",
+                many=False,
+                data=dict(name="foo", AGE=10),
+                expected=MarshalResult(data=dict(wrap=dict(name="foo", age=10)), errors={})
+            ),
+            C(
+                msg="people ok",
+                many=True,
+                data=[dict(name="foo", AGE=10), dict(name="bar", AGE=10)],
+                expected=MarshalResult(
+                    [dict(wrap=dict(name="foo", age=10)),
+                     dict(wrap=dict(name="bar", age=10))],
+                    errors={}
+                )
+            ),
+        ]
+        s = S()
+        for c in candidates:
+            with self.subTest(msg=c.msg):
+                got = s.dump(c.data, many=c.many)
+                self.assertEqual(got.errors, c.expected.errors)
+                self.assertEqual(got.data, c.expected.data)
