525fb410f23cd495ef2d92ef42696c513555d8ae
podhmo <ababjam61+github@gmail.com>
2018-06-05 20:26:28 +0900

    test: fix test template, using data_key

--- swagger_marshmallow_codegen/tests/dst/00emojis.py
+++ swagger_marshmallow_codegen/tests/dst/00emojis.py
@@ -5,8 +5,8 @@ from marshmallow import (
 
 
 class Emojis(Schema):
-    n100 = fields.String(dump_to='100', load_from='100')
-    n1234 = fields.String(dump_to='1234', load_from='1234')
-    n1 = fields.String(dump_to='1', load_from='1')
-    x_1 = fields.String(dump_to='-1', load_from='-1')
-    n8ball = fields.String(dump_to='8ball', load_from='8ball')
+    n100 = fields.String(data_key='100')
+    n1234 = fields.String(data_key='1234')
+    n1 = fields.String(data_key='1')
+    x_1 = fields.String(data_key='-1')
+    n8ball = fields.String(data_key='8ball')
--- swagger_marshmallow_codegen/tests/dst/00reserved.py
+++ swagger_marshmallow_codegen/tests/dst/00reserved.py
@@ -5,7 +5,7 @@ from marshmallow import (
 
 
 class Reserved(Schema):
-    not_ = fields.Boolean(dump_to='not', load_from='not')
-    as_ = fields.String(dump_to='as', load_from='as')
-    in_ = fields.List(fields.String(), dump_to='in', load_from='in')
-    yield_ = fields.String(dump_to='yield', load_from='yield')
+    not_ = fields.Boolean(data_key='not')
+    as_ = fields.String(data_key='as')
+    in_ = fields.List(fields.String(), data_key='in')
+    yield_ = fields.String(data_key='yield')
