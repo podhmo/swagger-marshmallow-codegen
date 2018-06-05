3ea4f4b0f07c7079d8855b9e7822f9eb267fe141
podhmo <ababjam61+github@gmail.com>
2018-06-05 19:48:20 +0900

    docs: removing, strict=true in examples

--- examples/02default/main.py
+++ examples/02default/main.py
@@ -5,7 +5,7 @@ from definition import Default
 if __name__ == "__main__":
     try:
         d = {}
-        data, err = Default(strict=True).load(d)
+        data, err = Default().load(d)
         print("ok", data)
     except Exception as e:
         print("ng", e)
