4870ba268d99187f0478ed6cfed2ac12cd9eeada
podhmo <ababjam61+github@gmail.com>
2018-06-05 20:49:01 +0900

    test: fix test template, using marshmallow's DatetimeField

--- swagger_marshmallow_codegen/tests/dst/00default.py
+++ swagger_marshmallow_codegen/tests/dst/00default.py
@@ -3,7 +3,6 @@ from marshmallow import (
     fields
 )
 import datetime
-from swagger_marshmallow_codegen.fields import DateTime
 from collections import OrderedDict
 
 
@@ -11,7 +10,7 @@ class X(Schema):
     string = fields.String(missing=lambda: 'default')
     integer = fields.Integer(missing=lambda: 10)
     boolean = fields.Boolean(missing=lambda: True)
-    datetime = DateTime(missing=lambda: datetime.datetime(2000, 1, 1, 1, 1, 1))
+    datetime = fields.DateTime(missing=lambda: datetime.datetime(2000, 1, 1, 1, 1, 1))
     object = fields.Nested('XObject', missing=lambda: OrderedDict([('name', 'foo'), ('age', 20)]))
     array = fields.List(fields.Integer(), missing=lambda: [1, 2, 3])
 
