f797d3b05b00bab890685b7929092c54251c84b1
podhmo <ababjam61+github@gmail.com>
2020-11-01 03:16:13 +0900

    chore: fist prototype

---
+++ shapes/defs/person.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import typing as t
+
+
+class Person:
+    name: str
+    age: t.Optional[int]
---
+++ shapes/gen.py
@@ -0,0 +1,34 @@
+import pathlib
+import logging
+from handofcats import as_command
+from metashape.outputs.openapi import emit, scan
+from metashape.runtime import get_walker
+from magicalimport import import_module
+
+logger = logging.getLogger(__name__)
+logging.getLogger("metashape.analyze.walker").setLevel(logging.WARNING)
+here = pathlib.Path(__file__).parent
+
+
+@as_command
+def run(filename: str, *, aggressive: bool = True) -> None:
+    m = import_module(filename, cwd=True)
+    walker = get_walker(m, aggressive=aggressive)
+    walker.config.option.strict = False
+    ctx = scan(walker)
+
+    name = pathlib.Path(filename).with_suffix(".json").name
+
+    # v3
+    with (here / "v3" / name).open("w") as wf:
+        logger.info("write %s", wf.name)
+        ctx.result["version"] = "3.x.x"
+        emit(ctx, output=wf)
+
+    # v2
+    with (here / "v2" / name).open("w") as wf:
+        logger.info("write %s", wf.name)
+        ctx.result["version"] = "2.0.0"
+        ctx.result["definitions"] = ctx.result["components"].pop("schemas")
+        ctx.result.pop("components")
+        emit(ctx, output=wf)
