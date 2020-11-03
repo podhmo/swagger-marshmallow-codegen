c97751708ebe8a939259e00840bd4000e140fe2d
podhmo <ababjam61+github@gmail.com>
2020-11-03 19:43:37 +0900

    feat: support header_comment option

--- swagger_marshmallow_codegen/codegen/config.py
+++ swagger_marshmallow_codegen/codegen/config.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 import typing_extensions as tx
 
 
-class ConfigDict(tx.TypedDict):
+class ConfigDict(tx.TypedDict, total=False):
     schema: bool
     input: bool
     output: bool
 
     emit_schema_even_primitive_type: bool
     skip_header_comment: bool
+    header_comment: str
