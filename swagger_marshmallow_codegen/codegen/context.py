64fea5b82c3e60086d44329b24ce7c11eb8f15b4
podhmo <ababjam61+github@gmail.com>
2020-11-03 17:58:55 +0900

    refactor: remove import_unique (#69)

--- swagger_marshmallow_codegen/codegen/context.py
+++ swagger_marshmallow_codegen/codegen/context.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 class Context:
     def __init__(self, m=None, im=None):
-        self.m = m or Module(import_unique=True)
+        self.m = m or Module()
         self.im = im or self.m.submodule()
 
     def from_(self, module, name):
