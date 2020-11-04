67d2c1d29ba40e09719e4f7ebf39482d93374b14
podhmo <ababjam61+github@gmail.com>
2020-11-04 09:03:31 +0900

    Readme markdown (#78)
    * doc: rst -> markdown
    
    * doc: rm readme.rst
    
    * chore: update setup.py,setup.cfg
    
    * chore: add tasks for handling package
    
    * doc: readme update badges

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-try:
-    with open(os.path.join(here, "README.rst")) as f:
-        README = f.read()
-    with open(os.path.join(here, "CHANGES.txt")) as f:
-        CHANGES = f.read()
-except IOError:
-    README = CHANGES = ""
 
 install_requires = [
     "dictknife[load]",
@@ -26,21 +15,17 @@ tests_require = ["pytest"]
 testing_extras = tests_require + []
 
 setup(
-    name="swagger-marshmallow-codegen",
-    version="0.5.1",
-    description="generating marshmallow's schema from swagger definition file",
-    long_description=README + "\n\n" + CHANGES,
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
     ],
     keywords="swagger,codegen,marshmallow,code-generation,schema",
-    author="podhmo",
-    author_email="ababjam61@gmail.com",
-    url="https://github.com/podhmo/swagger-marshmallow-codegen",
     packages=find_packages(exclude=["swagger_marshmallow_codegen.tests"]),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=install_requires,
     extras_require={
         "dev": ["black", "flake8"],
@@ -49,8 +34,4 @@ setup(
     },
     tests_require=tests_require,
     test_suite="swagger_marshmallow_codegen.tests",
-    entry_points="""
-      [console_scripts]
-swagger-marshmallow-codegen=swagger_marshmallow_codegen.cmd:main
-""",
 )
