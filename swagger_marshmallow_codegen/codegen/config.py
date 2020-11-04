43df12ab66fbfd00171605b4a7443678787abdb4
podhmo <ababjam61+github@gmail.com>
2020-11-04 12:44:58 +0900

    doc: comment

--- swagger_marshmallow_codegen/codegen/config.py
+++ swagger_marshmallow_codegen/codegen/config.py
@@ -3,13 +3,13 @@ import typing_extensions as tx
 
 
 class ConfigDict(tx.TypedDict, total=False):
-    schema: bool
-    input: bool
-    output: bool
+    schema: bool  # emit definitions schemas
+    input: bool  # emit input schema
+    output: bool  # emit output schema
 
     explicit: bool  # emit Meta.unknown=RAISE, explicitly
-    additional_properties_default: bool
+    additional_properties_default: bool  # if true meta.unknown=INCLUDE
 
-    emit_schema_even_primitive_type: bool
+    emit_schema_even_primitive_type: bool  # emit not used toplevel definitions
     skip_header_comment: bool
     header_comment: str
