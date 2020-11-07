e60527a8da033586e70c3316f159477231b2e63f
podhmo <ababjam61+github@gmail.com>
2020-11-07 15:24:43 +0900

    refactor: remove OptionsDict

--- shapes/try.py
+++ shapes/try.py
@@ -6,9 +6,9 @@ if t.TYPE_CHECKING:
 
 
 def setup(driver: Driver) -> None:
-    driver.options["targets"]["additional_properties_default"] = False
-    driver.options["targets"]["emit_schema_even_primitive_type"] = True
-    driver.options["targets"]["header_comment"] = ""
+    driver.config["additional_properties_default"] = False
+    driver.config["emit_schema_even_primitive_type"] = True
+    driver.config["header_comment"] = ""
 
 
 if __name__ == "__main__":
